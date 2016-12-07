296714dc1c39f48e52495e65f364905ed14b9fc7
Mmojoy <mmojoy.net@gmail.com>
2016-12-07 22:53:16 +0200

    todo + dragndrope + storage

--- lesson7/todo-jquery/js/main.js
+++ lesson7/todo-jquery/js/main.js
@@ -1,7 +1,16 @@
-  $( function() {
-    $( "#sortable" ).sortable();
-    $( "#sortable" ).disableSelection();
-  } );
+$(document).ready(function(){
+	$( "#sortable" ).sortable();
+
+
+});
+
+$( window ).load(function() {
+
+	if(localStorage.getItem('todos')) {
+		$('.collection').addClass("active").html(localStorage.getItem('todos'));
+		$(".card-panel").hide();
+	}
+});
 
 $(".submit-btn").on("click", function(){
 
@@ -13,7 +22,7 @@ $(".submit-btn").on("click", function(){
 		$(".card-panel").hide();
 		$(".btn-holder").addClass("active");
 
-		var inputInner = $("<input>").attr("type", "text").attr("disabled", true).val(listValue);
+		var inputInner = $("<input>").attr("type", "text").attr("disabled", true).attr("value", listValue);
 		inputInner.addClass("input");
 
 		var id = makeid();
@@ -32,6 +41,11 @@ $(".submit-btn").on("click", function(){
 
 		$(".collection").addClass("active").append(li);
 
+		var todos = $('.collection').html();
+
+		console.log(todos);
+		localStorage.setItem('todos', todos);
+
 	} else {
 		alert("Please, add smth!");
 	}
@@ -44,9 +58,13 @@ $(".collection").on("click", ".btn-trash", function(){
 
 $(".collection").on("click", ".btn-edit", function(){
 
+	var valIn = $(this).siblings(".input").attr("value");
+
+	// console.log(valIn);
+
 	if ($(this).hasClass("btn-save")) {
 		$(this).removeClass("btn-save").addClass("btn-edit").text("edit");
-		$(this).siblings(".input").attr("disabled", true);
+		$(this).siblings(".input").attr("disabled", true).attr("value", valIn);
 
 	} else {
 		$(this).siblings(".input").attr("disabled", false).focus();
@@ -64,6 +82,13 @@ $(".collection").on('click', 'input[type="checkbox"]', function(){
 	var row = $(this).parents('.collection-item');
 	$(this).is(':checked') ? row.addClass('checked') : row.removeClass('checked');
 
+});
+
+//clear localStorage
+$('#clear').click( function() {
+	window.localStorage.clear();
+	location.reload();
+	return false;
 });
 
 
