ea078c364eccf77d1321964b72c48047580dd040
in4eg <in4eg@mail.ua>
2016-12-10 21:37:58 +0200

    15

---
+++ 15/main.js
@@ -0,0 +1,15 @@
+/*
+*
+* Игра Пятнашки
+*
+* requires:   jQuery 1.6.4 or later
+*
+* author:     R
+* email:      r.rozinko@gmail.com
+* icq:        91 599 599
+* web-site:   http://livechip.net/
+*
+* Copyright 2014 LiveChip.Net
+*
+*/
+
