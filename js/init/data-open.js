47ac0d548ed685e6ec6c7cc29e3d057a97d4dcf9
in4eg <in4egsmile@gmail.com>
2018-04-23 22:20:19 +0300

    fix issues

--- js/init/data-open.js
+++ js/init/data-open.js
@@ -1,4 +1,14 @@
 $('[data-open]').click(function() {
-  $($(this).data('open')).find('.hidden').removeClass('hidden');
-  $(this).fadeOut();
+  var parent;
+  parent = $($(this).data('open')).find('[data-hidden]');
+  if (parent.hasClass('hidden')) {
+    // $('html,body').animate { scrollTop: parent.offset().top - 30 }, 200
+    $(this).addClass('active');
+    $($(this).data('open')).addClass('opened');
+    parent.removeClass('hidden');
+  } else {
+    parent.addClass('hidden');
+    $($(this).data('open')).removeClass('opened');
+    $(this).removeClass('active');
+  }
 });
