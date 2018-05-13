f994d2c33c72078c7a3ae7b3d6868cb015439037
in4eg <in4eg@mail.ua>
2018-05-13 13:20:27 +0300

    remove

--- js/init/header-scroll.js
+++
@@ -1,7 +0,0 @@
-$(document).on('scroll', function() {
-  if ($(window).scrollTop() > 0) {
-    $('.main-header').addClass('scrolled');
-  } else {
-    $('.main-header').removeClass('scrolled');
-  }
-});
