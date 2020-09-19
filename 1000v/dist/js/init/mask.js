b73bbece5232a68c03837b7df56c3ac49c21d32e
Inna <in4egsmile@gmail.com>
2020-09-19 20:03:20 +0300

    clear

--- 1000v/dist/js/init/mask.js
+++
@@ -1,4 +0,0 @@
-$('[data-mask]').each(function(i, input) {
-	var mask = $(input).attr('data-mask');
-	$(input).mask(mask, { placeholder: '' } );
-});
--- 1000v/dist/js/init/preloader.js
+++
@@ -1,7 +0,0 @@
-$(window).load(function(){
-	setTimeout(function(){
-		$('.preloader').each(function(i, preloader){
-			$(preloader).removeClass('active');
-		})
-	}, 350);
-});
