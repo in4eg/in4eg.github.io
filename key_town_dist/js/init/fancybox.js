37e0cebbdd3681f2928ee80c3af137bb7d064052
Inna <in4egsmile@gmail.com>
2024-03-03 22:06:42 +0200

    key town upload
    no api key

---
+++ key_town_dist/js/init/fancybox.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	if ($('[data-fancybox]')) {
+		Fancybox.bind('.owl-item:not(.cloned) [data-fancybox]', {
+			compact: false,
+			idle: false,
+			animated: false,
+			showClass: false,
+			hideClass: false,
+			dragToClose: false,
+			Images: {
+				zoom: false,
+			},
+			Toolbar: {
+				display: {
+					left: [],
+					middle: [],
+					right: ['close'],
+				},
+			},
+		});
+	};
+});
