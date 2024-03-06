8e8883f581b4af74750f69b94199c5d4872241a7
Inna <in4egsmile@gmail.com>
2024-03-06 18:43:05 +0200

    Delete key_town directory

--- key_town/js/init/scrollto.js
+++
@@ -1,9 +0,0 @@
-$('[data-scrollto]').click(function(e) {
-	var headerOffset;
-	console.log(e)
-	e.preventDefault();
-	headerOffset = $('.main-header').hasClass('fixed') ? $('.main-header').outerHeight() : 0;
-	$('html,body').animate({
-		scrollTop: $($(this).data('scrollto')).offset().top - headerOffset
-	}, 500);
-});
