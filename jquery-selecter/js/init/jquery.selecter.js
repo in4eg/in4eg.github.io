b1b51c952ac464f1f4716d20b87722df821feef8
Inna Kravchenko <in4egsmile@gmail.com>
2024-03-14 21:58:22 +0200

    test version
    selecter

---
+++ jquery-selecter/js/init/jquery.selecter.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	function getSelectChangedOption(select){
+		let id = $(select).attr('id');
+		filterArray = [];
+		$(select).find('option').each(function(i, option){
+			if ($(option).attr('selected') && !$(option).attr('disabled')) {
+				filterArray.push($(option).text().trim())
+			};
+		});
+		console.log(filterArray)
+	};
+
+	// select init
+	$(document).find('select').each(function(i, select) {
+		let data = $(select).data();
+		// selecter
+		$(select).selecter({
+			multiple: data && data.multiple === true ? true : false,
+			classname: data && data.classname ? data.classname : null,
+			onOpen: function(){ },
+			onChange: function(select){
+				getSelectChangedOption(select);
+			},
+			onClose: function(){ },
+		});
+	});
+
+});
