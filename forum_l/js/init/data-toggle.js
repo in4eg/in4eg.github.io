9efc66bff4b237bf3da126618bc8af0fd4fea8e4
Inna Kravchenko <in4egsmile@gmail.com>
2024-06-06 14:21:28 +0300

    menu toglle fix

--- forum_l/js/init/data-toggle.js
+++ forum_l/js/init/data-toggle.js
@@ -3,11 +3,17 @@ $('[data-toggle]').each(function(i, button) {
 		if ($(this).hasClass('active')) {
 			$(this).removeClass('active');
 			$('#'+$(this).data('toggle')).removeClass('active animated');
+			if ($(button).hasClass('button-menu')) {
+				$('body').removeClass('overlayed');
+			}
 		} else {
 			$(this).addClass('active');
 			$('#'+$(this).data('toggle')).addClass('active');
 			setTimeout(function(){
 				$('#'+$(this).data('toggle')).addClass('animated');
+				if ($(button).hasClass('button-menu')) {
+					$('body').addClass('overlayed');
+				}
 			}.bind(this), 200);
 		}
 	});
--- forum_l/js/init/scrollto.js
+++ forum_l/js/init/scrollto.js
@@ -1,9 +1,13 @@
 $('[data-scrollto]').click(function(e) {
 	var headerOffset;
-	console.log(e)
 	e.preventDefault();
 	headerOffset = $('.main-header').hasClass('fixed') ? $('.main-header').outerHeight() : 0;
 	$('html,body').animate({
 		scrollTop: $($(this).data('scrollto')).offset().top - headerOffset
 	}, 500);
+	if (window.innerWidth <= 1200 && $('#mainNavigation').hasClass('active')) {
+		$('#mainNavigation').removeClass('active animated');
+		$('.button-menu').removeClass('active');
+		$('body').removeClass('overlayed');
+	}
 });
