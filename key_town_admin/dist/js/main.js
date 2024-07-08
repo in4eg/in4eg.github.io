b6693bd4b91cdee242a13c1ec7bcb4047a97b7e4
Inna Kravchenko <in4egsmile@gmail.com>
2024-07-08 15:35:23 +0300

    modals

--- key_town_admin/dist/js/main.js
+++ key_town_admin/dist/js/main.js
@@ -298,6 +298,34 @@ $(document).ready(function() {
 	});
 });
 $(document).ready(function(){
+
+	function getSelectChangedOption(select){
+		let id = $(select).attr('id');
+		filterArray = [];
+		$(select).find('option').each(function(i, option){
+			if ($(option).attr('selected') && !$(option).attr('disabled')) {
+				filterArray.push($(option).text().trim())
+			};
+		});
+	};
+
+	// select init
+	$(document).find('select').each(function(i, select) {
+		let data = $(select).data();
+		// selecter
+		$(select).selecter({
+			multiple: data && data.multiple === true ? true : false,
+			classname: data && data.classname ? data.classname : null,
+			onOpen: function(){ },
+			onChange: function(select){
+				getSelectChangedOption(select);
+			},
+			onClose: function(){ },
+		});
+	});
+
+});
+$(document).ready(function(){
 	$(function() {
 
 		var setPointPosition = function(point, event) {
