e7402c3a74ddb06295479e46ddaf4f8af35bf203
Inna Kravchenko <in4egsmile@gmail.com>
2024-07-12 11:58:14 +0300

    file floader

--- key_town_admin/dist/js/main.js
+++ key_town_admin/dist/js/main.js
@@ -333,6 +333,56 @@ $(document).ready(function() {
 		});
 	};
 });
+function readfiles(files) {
+		console.warn(files)
+
+	for (var i = 0; i < files.length; i++) {
+		// document.getElementById('fileDragName').value = files[i].name
+		// document.getElementById('fileDragSize').value = files[i].size
+		// document.getElementById('fileDragType').value = files[i].type
+		// reader = new FileReader();
+		// reader.onload = function(event) {
+		// 	document.getElementById('fileDragImage').setAttribute('src', event.target.result)
+		// }
+		// reader.readAsDataURL(files[i]);
+	}
+}
+
+var fileLoader = document.getElementById('fileLoader');
+		fileLoader.ondragover = function (e) {
+			e.preventDefault();
+			fileLoader.classList.add('hover')
+		};
+		fileLoader.ondragend = function (e) {
+			e.preventDefault();
+			fileLoader.classList.remove('hover');
+		};
+		fileLoader.ondrop = function (e) {
+			e.preventDefault();
+			fileLoader.classList.remove('hover');
+			readfiles(e.dataTransfer.files);
+		}
+
+function getBase64(file) {
+	var reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = function () {
+		console.log(reader.result);
+	};
+	reader.onerror = function (error) {
+		console.log('Error: ', error);
+	};
+}
+
+function handleFiles() {
+	for (var i = 0; i < this.files.length; i++) {
+		console.warn(getBase64(this.files[i]))
+	}
+}
+
+var inputElement = document.getElementById("editObjFiles");
+			inputElement.addEventListener("change", handleFiles, false);
+
 $(document).ready(function(){
 
 	function getSelectChangedOption(select){
