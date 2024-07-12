b706a488f5bb6169d1b1ba06d17d9087776ce26a
Inna Kravchenko <in4egsmile@gmail.com>
2024-07-12 15:50:12 +0300

    loader

--- key_town_admin/dist/js/main.js
+++ key_town_admin/dist/js/main.js
@@ -333,55 +333,114 @@ $(document).ready(function() {
 		});
 	};
 });
-function readfiles(files) {
-		console.warn(files)
-
-	for (var i = 0; i < files.length; i++) {
-		// document.getElementById('fileDragName').value = files[i].name
-		// document.getElementById('fileDragSize').value = files[i].size
-		// document.getElementById('fileDragType').value = files[i].type
-		// reader = new FileReader();
-		// reader.onload = function(event) {
-		// 	document.getElementById('fileDragImage').setAttribute('src', event.target.result)
-		// }
-		// reader.readAsDataURL(files[i]);
-	}
-}
-
-var fileLoader = document.getElementById('fileLoader');
-		fileLoader.ondragover = function (e) {
-			e.preventDefault();
-			fileLoader.classList.add('hover')
-		};
-		fileLoader.ondragend = function (e) {
-			e.preventDefault();
-			fileLoader.classList.remove('hover');
-		};
-		fileLoader.ondrop = function (e) {
-			e.preventDefault();
-			fileLoader.classList.remove('hover');
-			readfiles(e.dataTransfer.files);
-		}
 
 function getBase64(file) {
 	var reader = new FileReader();
-	reader.readAsDataURL(file);
-	reader.onload = function () {
-		console.log(reader.result);
-	};
-	reader.onerror = function (error) {
-		console.log('Error: ', error);
-	};
-}
+	var errorStatus = {
+		errorLoad: 'Помилка завантаження',
+		errorType: 'Не підходящий формат для фото чи відео'
+	}
 
-function handleFiles() {
-	for (var i = 0; i < this.files.length; i++) {
-		console.warn(getBase64(this.files[i]))
+	reader.readAsDataURL(file);
+	if (file.type.startsWith("image/")) {
+		reader.onload = function () {
+			appendImage(reader.result);
+		};
+		reader.onerror = function (error) {
+			console.error('Error: ', error);
+		};
+	} else if (file.type.startsWith("video/")) {
+		reader.onload = function () {
+			appendVideo(reader.result);
+		};
+		reader.onerror = function (error) {
+			console.error('Error: ', error);
+			appendError(errorStatus.errorLoad);
+		};
+	} else {
+		console.error('Error: ', errorStatus.errorType);
+		appendError(errorStatus.errorType);
+	};
+};
+
+function appendImage(src){
+	let coverEl = document.createElement("div");
+			coverEl.classList.add('files-cover');
+	let imgEl = document.createElement('img');
+			imgEl.src = src;
+	coverEl.appendChild(imgEl);
+	loaderMainWrap.appendChild(coverEl);
+};
+
+function appendVideo(src){
+	let coverEl = document.createElement("div");
+			coverEl.classList.add('files-cover');
+	let videoEl = document.createElement('video')
+			videoEl.src = src
+			videoEl.controls = true;
+	coverEl.appendChild(videoEl);
+	loaderMainWrap.appendChild(coverEl)
+};
+
+function appendError(text){
+	let coverEl = document.createElement("div");
+			coverEl.classList.add('error-cover');
+			coverEl.appendChild(document.createTextNode(text));
+	loaderMainWrap.appendChild(coverEl);
+};
+
+function handleFiles(){
+	let files = this.files;
+	for (var i = 0; i < files.length; i++) {
+		getBase64(files[i]);
 	}
 }
 
-var inputElement = document.getElementById("editObjFiles");
-			inputElement.addEventListener("change", handleFiles, false);
+function hasClass(element, className) {
+	return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+}
+
+document.querySelectorAll('[data-loader]').forEach(function(loaderCover, i){
+	coverChildren = loaderCover.children;
+	for (var i = 0; i < coverChildren.length; i++) {
+		if (coverChildren[i].hasAttribute('data-label')) {
+			let fileLoader = coverChildren[i];
+					fileLoader.ondragover = function (e) {
+						e.preventDefault();
+						fileLoader.classList.add('hover')
+					};
+					fileLoader.ondragend = function (e) {
+						e.preventDefault();
+						fileLoader.classList.remove('hover');
+					};
+					fileLoader.ondrop = function (e) {
+						e.preventDefault();
+						fileLoader.classList.remove('hover');
+						for (var i = 0; i < e.dataTransfer.files.length; i++) {
+							getBase64(e.dataTransfer.files[i]);
+						}
+					}
+		} else if (coverChildren[i].hasAttribute('data-file')) {
+			let inputElement = coverChildren[i];
+					inputElement.addEventListener("change", handleFiles, false);
+
+		}
+	}
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function(){
 
