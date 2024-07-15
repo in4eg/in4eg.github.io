fe46d4cbcecf7d76471112af9211e82e164bd33c
Inna Kravchenko <in4egsmile@gmail.com>
2024-07-15 16:00:33 +0300

    login page

--- key_town_admin/dist/js/main.js
+++ key_town_admin/dist/js/main.js
@@ -736,41 +736,31 @@ $('body').on('click', '[data-tabs] > .tab', function(e) {
 	$('> .tab-content', container).removeClass('active').eq($(this).index()).addClass('active');
 });
 
-
-
 $(function() {
 	var isValidEmail;
 	$('.validate-form input:not(.ignored), .validate-form textarea:not(.ignored)').on('keyup keydown change', function() {
-		var errors, i, input;
-		input = this;
-		i = 0;
-		errors = [false];
+		let input = this;
+		let i = 0;
+		let errors = [false];
 		$(input).siblings('.icon-success').removeClass('active');
 		if ($(input).hasClass('email') && !isValidEmail($(input).val().trim())) {
 			errors[i] = true;
-			$(input).siblings('.icon-error').addClass('active');
-			$(input).parents('.input-cover').addClass('error');
+			$(input).parents('.input-cover').removeClass('success').addClass('error');
 		} else if ($(input).data('mask') && ($(input).val().trim().replace(/_/gim, '').length < $(input).data('mask').length)) {
 			errors[i] = true;
-			$(input).siblings('.icon-error').addClass('active');
-			$(input).parents('.input-cover').addClass('error');
+			$(input).parents('.input-cover').removeClass('success').addClass('error');
 		} else if ($(input).data('minlength') && $(input).val().trim().length < $(input).data('minlength')) {
 			errors[i] = true;
-			$(input).siblings('.icon-error').addClass('active');
-			$(input).parents('.input-cover').addClass('error');
+			$(input).parents('.input-cover').removeClass('success').addClass('error');
 		} else if ($(input).data('equals') && $(input).val().trim() !== $($(input).data('equals')).val().trim()) {
 			errors[i] = true;
-			$(input).siblings('.icon-error').addClass('active');
-			$(input).parents('.input-cover').addClass('error');
+			$(input).parents('.input-cover').removeClass('success').addClass('error');
 		} else if ($(input).val().trim() === "") {
 			errors[i] = true;
-			$(input).siblings('.icon-error').addClass('active');
 			$(input).parents('.input-cover').addClass('error');
 		} else {
 			errors[i] = false;
-			$(input).siblings('.icon-error').removeClass('active');
-			$(input).siblings('.icon-success').addClass('active');
-			$(input).parents('.input-cover').removeClass('error');
+			$(input).parents('.input-cover').removeClass('error').addClass('success');
 		}
 	});
 	isValidEmail = function(email) {
@@ -779,66 +769,43 @@ $(function() {
 		return re.test(email);
 	};
 	$('.validate-form').on('submit', function(e) {
-		var error, errors, hasErrors, isFormScrolled, j, len, scrollTarget;
+		var error, errors, hasErrors, j, len;
 		errors = [false, false];
-		isFormScrolled = false;
-		scrollTarget = $(this).parents('.popup').length !== 0 ? $('.popup') : $('html, body');
 		$('input:not(.ignored), textarea:not(.ignored)', this).each(function(i, input) {
 			$(input).siblings('.icon-success').removeClass('active');
 			if ($(input).hasClass('email') && !isValidEmail($(input).val().trim())) {
 				errors[i] = true;
-				$(input).siblings('.icon-error').addClass('active');
+				e.preventDefault();
 				$(input).parents('.input-cover').addClass('error');
 			} else if ($(input).data('minlength') && $(input).val().trim().length < $(input).data('minlength')) {
 				errors[i] = true;
-				$(input).siblings('.icon-error').addClass('active');
+				e.preventDefault();
 				$(input).parents('.input-cover').addClass('error');
 			} else if ($(input).data('equals') && $(input).val().trim() !== $($(input).data('equals')).val().trim()) {
 				errors[i] = true;
-				$(input).siblings('.icon-error').addClass('active');
+				e.preventDefault();
 				$(input).parents('.input-cover').addClass('error');
 			} else if ($(input).val().trim() === "") {
 				errors[i] = true;
-				$(input).siblings('.icon-error').addClass('active');
+				e.preventDefault();
 				$(input).parents('.input-cover').addClass('error');
 			} else {
 				errors[i] = false;
-				$(input).siblings('.icon-error').removeClass('active');
-				$(input).siblings('.icon-success').addClass('active');
 				$(input).parents('.input-cover').removeClass('error');
 			}
 		});
 		hasErrors = false;
-		for (j = 0, len = errors.length; j < len; j++) {
-			error = errors[j];
-			if (error) {
-				hasErrors = true;
-				e.preventDefault();
-				$('.form-head').addClass('error');
-				return;
-			}
-		}
-		if (!hasErrors) {
-			$('.success-fadeout', this).addClass('active');
-			$('.success-fadein', this).fadeIn();
-			$(input).parents('.input-cover').removeClass('error');
-		}
-	});
-	$('.file-input').each(function(i, input) {
-		$('input', input).on('change', function() {
-			return $('label', input).html(this.files[0].name);
-		});
 	});
 });
 
 window.addEventListener("DOMContentLoaded", function() {
-		[].forEach.call( document.querySelectorAll('.phone'), function(input) {
+		[].forEach.call( document.querySelectorAll('.input-phone'), function(input) {
 		var keyCode;
 		function mask(event) {
 				event.keyCode && (keyCode = event.keyCode);
 				var pos = this.selectionStart;
 				if (pos < 3) event.preventDefault();
-				var matrix = "+38 (___) ___-__-__",
+				var matrix = "__________",
 						i = 0,
 						def = matrix.replace(/\D/g, ""),
 						val = this.value.replace(/\D/g, ""),
@@ -865,3 +832,18 @@ window.addEventListener("DOMContentLoaded", function() {
 		input.addEventListener("keydown", mask, false)
 	});
 });
+
+
+$(document).ready(function() {
+	$('[data-show]').each(function(i, button){
+		$(button).click(function(e) {
+			if ($(this).hasClass('active')) {
+				$(this).removeClass('active');
+				$($(this).data('show')).find('[type="text"]').attr('type', 'password');
+			} else {
+				$(this).addClass('active');
+				$($(this).data('show')).find('[type="password"]').attr('type', 'text');
+			}
+		});
+	})
+})
