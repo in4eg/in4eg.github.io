812353b2bc0ca84bca0474655ff30cfe686294ed
Inna Kravchenko <in4egsmile@gmail.com>
2025-01-27 21:17:20 +0200

    elements

--- vybir/dist/js/init/buttons.js
+++ vybir/dist/js/init/buttons.js
@@ -8,14 +8,11 @@ document.addEventListener("DOMContentLoaded", function() {
 		fader.style.left = left + "px";
 		fader.style.top = top + "px";
 		setTimeout((function(_this) {
-			return function() {
-				return document.querySelector('.fade', _this).classList.add('active');
-			};
+			document.querySelector('.fade', _this).classList.add('active');
 		})(button), 1);
 		setTimeout((function(_this) {
-			return function() {
-				return document.querySelector('.fade', _this).classList.remove('active');
-			};
+			console.warn(document.querySelector('.fade', _this))
+			document.querySelector('.fade', _this).classList.remove('active');
 		})(button), 610);
 	}
 	// find all buttons
