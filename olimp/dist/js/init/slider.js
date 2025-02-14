5755d5fc86257f1eb6658303e702435e75934844
Inna Kravchenko <in4egsmile@gmail.com>
2025-02-14 10:09:17 +0200

    slider update

--- olimp/dist/js/init/slider.js
+++ olimp/dist/js/init/slider.js
@@ -35,7 +35,7 @@ window.addEventListener('load', function(){
 				}
 			},{
 				// screens greater than >= 775px
-				// breakpoint: 993,
+				breakpoint: 993,
 				settings: {
 					// Set to `auto` and provide item width to adjust to viewport
 					slidesToShow: 3,
@@ -57,7 +57,31 @@ window.addEventListener('load', function(){
 
 document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
 	var glider = Glider(this);
-	// console.warn(glider)
-	// console.warn(event)
+
+	let countCover = glider.ele.parentElement.querySelector('.slider-navigation .slider-counter');
+	if (!countCover) return;
+
+	let totalSlides = glider.slides.length;
+	let currentSlides = event.detail.slide+1;
+
+	countCover.querySelector('.current').innerHTML = currentSlides;
+	countCover.querySelector('.total').innerHTML = totalSlides;
+
+
+	let fadeWidth = glider.opt.slidesToScroll*10;
+
+	glider.ele.parentElement.querySelector('.slider-navigation .fade').style.width = fadeWidth + '%';
+
+	let leftPosition = (100 - (totalSlides - currentSlides)/totalSlides*100) - fadeWidth;
+	if (leftPosition < 0) {
+		leftPosition = 0
+	}
+
+
+
+
+	glider.ele.parentElement.querySelector('.slider-navigation .fade').style.left = leftPosition + '%';
+
+
 
 });
