e9fa1d47ed2b8f26c81e75bf890239f09a6b7c04
Inna <in4egsmile@gmail.com>
2025-08-09 22:47:40 +0300

    Delete iqos_retailer directory

--- iqos_retailer/dist/js/init/data-form-show.js
+++
@@ -1,7 +0,0 @@
-$('[data-form-show]').click(function(){
-	parent = $(this).data('form-show');
-	$(parent).addClass('active');
-	setTimeout(function(){
-		$(parent).addClass('animated');
-	}, 150);
-})
